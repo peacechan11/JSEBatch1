7a6e22adbcc74e0eb4ee32cb3cb2a6c0518b745d
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-08-07 20:46:43 +0700

    project init

---
+++ src/main/java/dev/peacechan/assignment1/SumofNumbers.java
@@ -0,0 +1,11 @@
+package dev.peacechan.assignment1;
+
+public class SumofNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of numbers from 1 to 10 is: " + sum);
+    }
+}
