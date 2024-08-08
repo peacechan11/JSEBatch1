de9da7f3946a3b505fb985aa8d3a734f4e85bb96
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-08-08 09:10:57 +0700

    added Multiplication

---
+++ src/main/java/dev/peacechan/assignment1/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package dev.peacechan.assignment1;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Multiplication Table for "+ number+" :");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
